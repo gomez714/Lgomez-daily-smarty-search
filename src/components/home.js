e8506115f09543af77345c7bc86011d2a884783b
Luis Gomez <luey714@gmail.com>
2022-01-21 17:17:15 -0600

    added search functionality from both the home page and results page

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Logo from './logo';
 import RecentPosts from './recent-posts';
 import Searchbar from './searchbar';
 
 const Home = () => {
+    let navigate = useNavigate();
+    const handleSearchSubmit = (query) => {
+        navigate('results', {state: `${query}`})
+    }
 
     return (
         <main className='home'>
             <Logo />
-            <Searchbar page='home'/>
+            <Searchbar page='home' onSubmit={(query) => handleSearchSubmit(query)}/>
             <RecentPosts />
         </main>
     )
--- src/components/results.js
+++ src/components/results.js
@@ -3,16 +3,24 @@ import axios from "axios";
 import Logo from "./logo";
 import Searchbar from "./searchbar";
 import ResultsPosts from "./results-posts";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react/cjs/react.development";
 
 const Results = () => {
-
     const [searchResults, setSearchResults] = useState([]);
+    
+    let location = useLocation();
+    
+    useEffect(() => {
+        fetchResults(location.state)
+        location.state = null;
+    }, [])
 
     const fetchResults = async (query) => {
         const res = await axios.get(`https://api.dailysmarty.com/search?q=${query}`)
         setSearchResults(res.data.posts);
     }
-    
+
     return (
         <div className="results">
             <Logo customSize={55} />
