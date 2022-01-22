13cdab286f01010420746b54d43eb28cbd22d607
Luis Gomez <luey714@gmail.com>
2022-01-22 12:08:06 -0600

    fix prod bug on deployment to firebase and removed unused imports

--- src/components/results.js
+++ src/components/results.js
@@ -1,25 +1,24 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Logo from "./logo";
 import Searchbar from "./searchbar";
 import ResultsPosts from "./results-posts";
 import { useLocation } from "react-router-dom";
-import { useEffect } from "react/cjs/react.development";
 
 const Results = () => {
     const [searchResults, setSearchResults] = useState([]);
     
     let location = useLocation();
     
-    useEffect(() => {
-        fetchResults(location.state)
-        location.state = null;
-    }, [])
-
     const fetchResults = async (query) => {
         const res = await axios.get(`https://api.dailysmarty.com/search?q=${query}`)
         setSearchResults(res.data.posts);
     }
+
+    React.useEffect(() => {
+        fetchResults(location.state);
+        location.state = null;
+    }, [])
 
     return (
         <div className="results">
